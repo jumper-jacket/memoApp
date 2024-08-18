f2c0ccf54daf6f57ac042b74df2ee6049cb00e93
User <u96464224@gmail.com>
2024-08-18 23:15:35 +0900

    The point at which refactoring begins because the code has become too complex.

--- memo/src/App.tsx
+++ memo/src/App.tsx
@@ -1,18 +1,46 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Sidebar from './components/Sidebar';
 import Content from './components/Content';
+import DeleteLocalStorage from './components/DeleteLocalStorage';
+
+const STORAGE_KEY = 'memoAppNotes';
+
+type Notes = {
+  id: number,
+  title: string,
+  content: string,
+};
 
 const App: React.FC = () => {
-  const [notes] = useState([
-    { id: 1, title: 'メモ 1', content: 'メモ 1 の内容' },
-    { id: 2, title: 'メモ 2', content: 'メモ 2 の内容' },
-    { id: 3, title: 'メモ 3', content: 'メモ 3 の内容' },
-  ]);
-
+  const [notes, setNotes] = useState<Notes[]>([]);
   const [selectedNote, setSelectedNote] = useState<number | null>(null);
+
+  useEffect(() => {
+    const savedNotes = localStorage.getItem(STORAGE_KEY);
+
+    if (!savedNotes) {
+      setNotes([
+        { id: 1, title: 'メモ 1', content: 'これはメモ 1 の内容です。' },
+        { id: 2, title: 'メモ 2', content: 'これはメモ 2 の内容です。' },
+        { id: 3, title: 'メモ 3', content: 'これはメモ 3 の内容です。' },
+      ]);
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(notes));
+  }, [notes]);
 
   const handleSelectNote = (id: number) => {
     setSelectedNote(id);
+  };
+
+  const handleSaveNote = (id: number, updatedTitle: string, updatedContent: string) => {
+    setNotes((prevNotes) =>
+      prevNotes.map((note) =>
+        note.id === id ? { ...note, title: updatedTitle, content: updatedContent } : note
+      )
+    );
   };
 
   const selectedNoteContent = notes.find((note) => note.id === selectedNote) || null;
@@ -20,7 +48,8 @@ const App: React.FC = () => {
   return (
     <div className="flex h-screen">
       <Sidebar notes={notes} onSelect={handleSelectNote} />
-      <Content note={selectedNoteContent}  onSave={handleSelectNote} />
+      <Content note={selectedNoteContent} onSave={handleSaveNote} />
+      <DeleteLocalStorage />
     </div>
   );
 };
---
+++ memo/src/components/AddMemo.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const ClearStorageButton: React.FC = () => {
+  const handleAddStorage = () => {
+    localStorage.removeItem('memoAppNotes');
+    window.location.reload();  
+  };
+
+  return (
+    <div className='flex flex-col-reverse'>
+        <button onClick={handleAddStorage} className="bg-red-500 text-white p-2 rounded">
+            ローカルストレージを削除
+        </button>
+    </div>
+
+  );
+};
+
+export default ClearStorageButton;
